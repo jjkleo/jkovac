99380214c254dc41ec8e264dcf24574b40b58d37
jjkleo <2472650191@qq.com>
2020-06-20 07:01:45 +0800

    分离环境配置

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,64 +1,13 @@
 const path=require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports={
+module.exports = merge(common,{
   mode:'development',
-  context: path.resolve(__dirname,'./'),//配置上下文
-  entry:'./src/main',
   output:{
     filename:'[name].js',
     chunkFilename:'[name].js',//非入口chunk的文件名称
-    path: path.resolve(__dirname,'dist'),
-    crossOriginLoading: false,//禁用跨域加载
   },
-  module: {
-    //忽略大型的 library
-    noParse: function(content) {
-      return /jquery|lodash/.test(content);
-    },
-    rules:[
-      {
-        test: /\.vue$/,
-        use:['vue-loader']
-      },
-      {
-        test:/\.ts/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-        options:{
-          appendTsSuffixTo: [/\.vue$/],
-        }
-      },
-      {
-        test:/\.js$/,
-        use:['babel-loader']
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-          'sass-loader'
-        ],
-        exclude: /node_modules/,
-      }
-    ]
-  },
-  resolve:{
-    alias: {
-      '@': path.resolve(__dirname,'src'),
-    },
-    mainFields: ["browser", "module", "main"],//第三方模块导入顺序
-    enforceExtension: false ,
-    extensions: ['.tsx','.ts','.js','.vue','.json',],
-    modules: ["node_modules"],//第三方模块位置
-    descriptionFiles: ["package.json"], //第三方模块描述文件
-  },
-  plugins:[
-    new VueLoaderPlugin(),
-  ],
-  target:'web', //针对web应用
   devtool:"eval-source-map ",//开发环境source map
   devServer:{
     host:'localhost',
@@ -80,4 +29,4 @@ module.exports={
   performance: {
     hints: false,//不展示提示
   }
-};
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,64 +1,20 @@
-const path=require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports={
+module.exports = merge(common,{
   mode:'production',
-  context: path.resolve(__dirname,'./'),//配置上下文
-  entry:'./src/main',
   output:{
     filename:'[name].[hash].js',
     chunkFilename:'[id].[hash].js',//非入口chunk的文件名称
-    path: path.resolve(__dirname,'dist'),
-    crossOriginLoading: false,//禁用跨域加载
-  },
-  module: {
-    //忽略大型的 library
-    noParse: function(content) {
-      return /jquery|lodash/.test(content);
-    },
-    rules:[
-      {
-        test: /\.vue$/,
-        use:['vue-loader']
-      },
-      {
-        test:/\.ts/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-        options:{
-          appendTsSuffixTo: [/\.vue$/],
-        }
-      },
-      {
-        test:/\.js$/,
-        use:['babel-loader']
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader'
-        ],
-        exclude: /node_modules/,
-      }
-    ]
-  },
-  resolve:{
-    alias: {
-      '@': path.resolve(__dirname,'src'),
-    },
-    mainFields: ["browser", "module", "main"],//第三方模块导入顺序
-    enforceExtension: false ,
-    extensions: ['.tsx','.ts','.js','.vue','.json',],
-    modules: ["node_modules"],//第三方模块位置
-    descriptionFiles: ["package.json"], //第三方模块描述文件
   },
   plugins:[
-    new VueLoaderPlugin(),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
       chunkFilename: '[id].[hash].css',
@@ -71,11 +27,17 @@ module.exports={
       favicon:"./public/favicon.ico",
     }),
   ],
-  target:'web', //针对web应用
-  // devtool:"eval-source-map ",//开发环境source map
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        parallel: true,
+      }),
+      new OptimizeCssAssetsPlugin({}),
+    ],
+  },
   performance: {
     hints: "warning",//性能提示
     maxAssetSize: 250000,//最大文件大小
     maxEntrypointSize: 250000//最大入口文件大小
   }
-};
+});
